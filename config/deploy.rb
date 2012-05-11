27e7851118f4aefb7a9a379bb1449b7a2de5ee14
Erik Grinaker <erik@bengler.no>
2012-05-11 14:30:07 +0200

    Use new Unicorn deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,8 @@ set :application, 'hermes'
 set :repository, 'git@github.com:bengler/hermes'
 set :stages, ['production', 'staging']
 set :runner, 'hermes'
+set :unicorn_config, '/srv/hermes/shared/config/unicorn.rb'
+set :unicorn_pid, '/srv/hermes/shared/pids/unicorn.pid'
 
 # Must be loaded after setting options
 require 'capistrano/bengler'
