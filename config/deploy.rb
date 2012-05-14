af482b26bc2d7b87402ddb701292aa66e6947eb7
Erik Grinaker <erik@origo.no>
2012-05-14 21:35:48 +0200

    Link realm configs from shared/ on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,10 @@ set :runner, 'hermes'
 set :unicorn_config, '/srv/hermes/shared/config/unicorn.rb'
 set :unicorn_pid, '/srv/hermes/shared/pids/unicorn.pid'
 
+after :prepare, :config
+task :config do
+  run "sudo -u #{runner} ln -sfT #{shared_path}/config/realms #{current_path}/config/realms"
+end
+
 # Must be loaded after setting options
 require 'capistrano/bengler'
