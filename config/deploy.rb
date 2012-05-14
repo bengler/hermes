ba3d96a3dc0218a444192ac8539255afdb8219bc
Erik Grinaker <erik@origo.no>
2012-05-14 20:25:49 +0200

    Removed staging stage.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'hermes'
 set :repository, 'git@github.com:bengler/hermes'
-set :stages, ['production', 'staging']
+set :stages, ['production']
 set :runner, 'hermes'
 set :unicorn_config, '/srv/hermes/shared/config/unicorn.rb'
 set :unicorn_pid, '/srv/hermes/shared/pids/unicorn.pid'
