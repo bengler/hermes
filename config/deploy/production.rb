bf191f6966c1677b5697ebea8479a4ddb7f1f227
Erik Grinaker <erik@bengler.no>
2012-07-13 02:30:43 +0200

    Deploy to new database servers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :rack_env, 'production'
 
 role :app, 'jara.park.origo.no', 'kivik.park.origo.no', 'lack.park.origo.no', 'marginal.park.origo.no', 'nominell.park.origo.no'
 role :web, 'lack.park.origo.no', 'marginal.park.origo.no', 'nominell.park.origo.no'
-role :db, 'lack.park.origo.no', :primary => true
-role :db, 'marginal.park.origo.no', 'nominell.park.origo.no'
+role :db, 'jara.park.origo.no', :primary => true
+role :db, 'kivik.park.origo.no'
