bde48fcb7f433ca1b170a891401f8f87d181c9d8
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2012-11-08 00:45:41 +0100

    Update message controller

--- api/v1/messages_controller.rb
+++ api/v1/messages_controller.rb
@@ -13,8 +13,12 @@ module Hermes
 
       register Sinatra::Pebblebed
 
-      error ::Hermes::Configuration::ProviderNotFound do
-        404
+      error ::Hermes::Configuration::ProviderNotFound do |e|
+        return halt 404, e.message
+      end
+
+      error ::Hermes::OptionMissingError do |e|
+        return halt 400, e.message
       end
 
       error StandardError, Exception do |e|
@@ -38,30 +42,32 @@ module Hermes
           LOGGER
         end
 
-        def receipt_url(profile)
+        def receipt_url(realm)
           # FIXME: Make configurable
           case ENV['RACK_ENV']
             when 'development'
               # Set up a tunnel on samla.park.origo.no port 10900 to receive receipts
-              "http://origo.tunnel.o5.no/api/hermes/v1/#{profile}/receipt"
+              "http://origo.tunnel.o5.no/api/hermes/v1/#{realm}/receipt"
             when 'staging'
-              "http://hermes.o5.no/api/hermes/v1/#{profile}/receipt"
+              "http://hermes.o5.no/api/hermes/v1/#{realm}/receipt"
             else
-              "http://hermes.staging.o5.no/hermes/v1/#{profile}/receipt"
+              "http://hermes.staging.o5.no/hermes/v1/#{realm}/receipt"
           end
         end
       end
 
-      get '/stats' do
-        pg :statistics, :locals => {:statistics => Message.statistics}
+      get '/:realm/stats/sms' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :sms)
+        pg :statistics, :locals => {:statistics => Message.statistics(:realm => realm, :kind => 'sms')}
       end
 
-      get '/:profile/stats' do |profile|
-        pg :statistics, :locals => {:statistics => Message.statistics(:profile => profile)}
+      get '/:realm/stats/email' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :email)
+        pg :statistics, :locals => {:statistics => Message.statistics(:realm => realm, :kind => 'email')}
       end
 
-      post '/:profile/test' do |profile|
-        provider = @configuration.provider_for_profile(profile)
+      post '/:realm/test/sms' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :sms)
         if provider.test!
           halt 200, "Provider is fine"
         else
@@ -69,21 +75,20 @@ module Hermes
         end
       end
 
-      get '/:profile/messages/:id' do |profile, id|
-        message = Message.where(:profile => profile).where(:id => id).first
-        if message
-          pg :message, :locals => {:message => message}
-        else
-          404
+      get '/:realm/messages/:id' do |realm, id|
+        message = Message.where(:id => id)
+        if message.any?
+          if message.first.realm == realm
+            return pg :message, :locals => {:message => message.first}
+          end
         end
+        halt 404, "No such message"
       end
 
-      post '/:profile/messages' do |profile|
-        provider = @configuration.provider_for_profile(profile)
-
+      post '/:realm/messages/sms' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :sms)
         attrs = JSON.parse(request.env['rack.input'].read)
-
-        id = provider.send_short_message!(
+        id = provider.send_message!(
           :recipient_number => attrs['recipient_number'],
           :sender_number => attrs['sender_number'],
           :rate => {
@@ -91,25 +96,48 @@ module Hermes
             :amount => (attrs['rate'] || {})['amount']
           },
           :body => attrs['body'],
-          :receipt_url => receipt_url(profile),
+          :receipt_url => receipt_url(realm),
           :bill => attrs['bill'])
 
         message = Message.create!(
           :vendor_id => id,
-          :profile => profile,
+          :realm => realm,
           :status => 'in_progress',
-          :recipient_number => attrs['recipient_number'],
+          :kind => 'sms',
+          :recipient => attrs['recipient_number'],
           :callback_url => attrs['callback_url'],
           :bill => attrs['bill'])
 
         response.status = 202
-        response.headers['Location'] = url("#{profile}/#{message.id}")
+        response.headers['Location'] = url("#{realm}/messages/sms/#{message.id}")
         response.headers['Content-Type'] = 'text/plain'
         message.id.to_s
       end
 
-      post '/:profile/receipt' do |profile|
-        provider = @configuration.provider_for_profile(profile)
+      post '/:realm/messages/email' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :email)
+        attrs = JSON.parse(request.env['rack.input'].read)
+        id = provider.send_message!(
+          :recipient_email => attrs['recipient_email'],
+          :sender_email => attrs['sender_email'],
+          :text => attrs['text'],
+          :html => attrs['html'],
+          :receipt_url => receipt_url(realm))
+        message = Message.create!(
+          :vendor_id => id,
+          :realm => realm,
+          :status => 'in_progress',
+          :kind => 'sms',
+          :recipient => attrs['recipient_email'],
+          :callback_url => attrs['callback_url'])
+        response.status = 202
+        response.headers['Location'] = url("#{realm}/messages/email/#{message.id}")
+        response.headers['Content-Type'] = 'text/plain'
+        message.id.to_s
+      end
+
+      post '/:realm/receipt' do |realm|
+        provider = @configuration.provider_for_realm_and_kind(realm, :sms)
         raw = request.env['rack.input'].read if request.env['rack.input']
         raw ||= ''
         begin
@@ -119,7 +147,7 @@ module Hermes
         else
           if result[:id] and result[:status]
             message = Message.
-              where(:profile => profile).
+              where(:realm => realm).
               where(:vendor_id => result[:id]).first
             if message
               message.status = result[:status]
