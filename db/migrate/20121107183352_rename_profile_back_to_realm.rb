f9ca31b4eb7d9af6439c96f8e5c228cbfeed4d52
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2012-11-09 12:37:13 +0100

    Remove database stuff

--- db/migrate/20121107183352_rename_profile_back_to_realm.rb
+++
@@ -1,9 +0,0 @@
-class RenameProfileBackToRealm < ActiveRecord::Migration
-  def self.up
-    rename_column :messages, :profile, :realm
-  end
-
-  def self.down
-    rename_column :messages, :realm, :profile
-  end
-end
--- db/migrate/20121107185650_rename_recipient_field.rb
+++
@@ -1,9 +0,0 @@
-class RenameRecipientField < ActiveRecord::Migration
-  def self.up
-    rename_column :messages, :recipient_number, :recipient
-  end
-
-  def self.down
-    rename_column :messages, :recipient, :recipient_number
-  end
-end
