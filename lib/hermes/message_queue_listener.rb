77cb01cd1f934cf365852e5408bcdf569235593e
Thomas Drevon <thomas@bengler.no>
2015-01-21 07:00:14 +0100

    Message queue listener up and running, with test

--- lib/hermes/message_queue_listener.rb
+++ lib/hermes/message_queue_listener.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
+require 'deepstruct'
 
 module Hermes
 
@@ -10,24 +11,47 @@ module Hermes
       nil
     end
 
+    # we only subscribe to new posts (event == 'create') so messages will only appear here once
     def consider(payload)
       puts("Consider #{payload}")
+      post = payload['attributes']
+      post = fix_tags!(post)
+      unless post['tags'].include? 'queued'
+        LOGGER.error("Message #{post['uid']} not queued. That's odd.")
+        return
+      end
+      puts("Consider #{post}")
 
-      # raw_message.delete(:callback_url)
-      #
-      # begin
-      #   id = @provider.send_message!(raw_message)
-      # rescue ProviderError
-      #   logger.info("Provider failed to send message (#{kind} via #{@provider.class.name}): #{message.inspect}")
-      #   grove_post[:tags] = ['failed']
-      #   pebblebed_connector(@realm, current_identity).grove.post(grove_path, post: grove_post)
-      #   raise
-      # else
-      #   logger.info("Sent message (#{kind} via #{@provider.class.name}): #{message.inspect}")
-      #   grove_post[:tags] = ['inprogress']
-      #   grove_post[:external_id] = Message.build_external_id(@provider, id)
-      #   pebblebed_connector(@realm, current_identity).grove.post(grove_path, post: grove_post).to_json
-      # end
+      message = post['document'].dup
+      message.delete('callback_url')
+
+      realm_name = Pebbles::Uid.new(post['uid']).realm
+      realm = CONFIG.realm(realm_name)
+      provider = realm.provider(message['kind'])
+      connector = realm.pebblebed_connector
+      grove_path = "/posts/#{post['uid']}"
+
+      begin
+        id = provider.send_message!(message)
+      rescue ProviderError
+        post['tags'] << 'failed'
+        connector.grove.post(grove_path, post: post)
+        raise
+      else
+        post['tags'] << 'inprogress'
+        post['external_id'] = Message.build_external_id(provider, id)
+        connector.grove.post(grove_path, post: post).to_json
+      end
+    end
+
+    private
+
+    def fix_tags!(post)
+      tags = post['tags_vector']
+      tags = tags.split('\' \'').map{|t| t.gsub('\'','')}
+      post['tags'] = tags
+      post.delete('tags_vector')
+      post
     end
 
   end
---
+++ spec/message_queue_listener_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+include Hermes
+
+describe 'MessageQueueListener' do
+
+  subject {
+    Hermes::MessageQueueListener.new
+  }
+
+  let(:uid) {
+    'post.hermes_message:test.call.me$xyzzy'
+  }
+
+  let(:sms_payload) {
+    {
+      'attributes' => {
+        'document' => {
+          'sender_number' => "555-callme",
+          'recipient_number' => "555-goaway",
+          'text' => 'sup',
+          'kind' => 'sms'
+        },
+        'uid' => uid,
+        'tags_vector' => "'queued'"
+      }
+    }
+  }
+
+  context "incoming message" do
+
+    it "is handled correctly" do
+      expected_path = "/posts/#{uid}"
+      expect_any_instance_of(Pebblebed::GenericClient).to receive(:post) do |path, hash|
+        path.should eq expected_path
+        hash[:post]['document'].should eq sms_payload['attributes']['document']
+        hash[:post]['tags'].should eq ["queued", "inprogress"]
+      end
+      subject.consider sms_payload
+    end
+
+  end
+
+end
